682fcb64ce42ae84a8202f6e65d8555ebcfc32ed
meowconkhocduoimua <79196638+meowconkhocduoimua@users.noreply.github.com>
2023-06-16 16:47:42 +0700

    Create 1223.py

---
+++ 1223.py
@@ -0,0 +1,24 @@
+{
+    "summary": "Drink eight glasses of water a day and relax your mind and body.",
+    "description": "Eat more fruits, exercise more, live a healthy life, and work happily.",
+    "rich_summary": "rich_summary[feishu open-platform](https://open.feishu.cn)",
+    "rich_description": "rich_description[feishu open-platform](https://open.feishu.cn)",
+    "extra": "dGVzdA==",
+    "due": {
+        "time": "1623124318",
+        "timezone": "Asia/Shanghai",
+        "is_all_day": false
+    },
+    "origin": {
+        "platform_i18n_name": "{\"zh_cn\": \"IT 工作台\", \"en_us\": \"IT Workspace\"}",
+        "href": {
+            "url": "https://support.feishu.com/internal/foo-bar",
+            "title": "We need assistance to resolve an issue"
+        }
+    },
+    "can_edit":true,
+    "custom": "{\"custom_complete\":{\"android\":{\"href\":\"https://www.feishu.cn/\",\"tip\":{\"zh_cn\":\"你好\",\"en_us\":\"hello\"}},\"ios\":{\"href\":\"https://www.feishu.cn/\",\"tip\":{\"zh_cn\":\"你好\",\"en_us\":\"hello\"}},\"pc\":{\"href\":\"https://www.feishu.cn/\",\"tip\":{\"zh_cn\":\"你好\",\"en_us\":\"hello\"}}}}",
+    "follower_ids": ["ou_13585843f02bc94923ed17a007cbc9b1", "ou_219a0611de2a639aa939ee97013f37a5"],
+    "collaborator_ids": ["ou_13585843f02bc94923ed17a007cbc9b1", "ou_219a0611de2a639aa939ee97013f37a5"],
+    "repeat_rule": "FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,TU,WE,TH,FR"
+}
